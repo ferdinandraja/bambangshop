41b80b86f0a139aa84b7eca37b3c5aa5233dbc7b
ferdinandraja <ferdinand.raja@ui.ac.id>
2024-04-14 17:18:49 +0700

    Create Notification model struct.

---
+++ src/model/notification.rs
@@ -0,0 +1,10 @@
+use rocket::sede::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Notification{
+    pub product_title: String,
+    pub product_type: String,
+    pub product_url: String,
+    pub subscriber_name: String,
+    pub status: String,
+}
